1fa9d5e30175896da415e298ceba4fc32e4ff849
peusterm <manuel.peuster@uni-paderborn.de>
2018-11-13 08:31:12 +0100

    More cleanup
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

---
+++ vnfs/rtr-quagga/vnf_source/README.md
@@ -0,0 +1,1 @@
+This folder contains the code/Dockerfile in case of Docker-based VNFs.
---
+++ vnfs/vpn/fsm/snort-config/tngfsm_css/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from tngfsm_css import tngfsm_css
+
+def main():
+    tngfsm_css.main()
+
+if __name__ == '__main__':
+    main()
