0e6822e79dce321842bbc5a003ad0b694da906df
peusterm <manuel.peuster@uni-paderborn.de>
2018-12-11 16:18:16 +0100

    First work towards using real CNFs for the pilot. #46
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- sdk-projects/pack.sh
+++ sdk-projects/pack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 # package all projects for pilot
-tng-pkg -p tng-smpilot-ns1-emulator
+tng-validate -s --project tng-smpilot-ns1-emulator/  # sytax validation only (as long as we have to skip integrety validation for CNFs)
+tng-pkg -p tng-smpilot-ns1-emulator  --skip-validation  # skip integrety validation until validator supports CNFs
