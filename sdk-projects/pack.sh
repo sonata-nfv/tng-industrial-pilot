a0ce2ff57f0bffb5c246af5d60363f06dd995a6d
peusterm <manuel.peuster@uni-paderborn.de>
2018-12-17 10:42:51 +0100

    Fix: Removed workaround from pack.sh
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- sdk-projects/pack.sh
+++ sdk-projects/pack.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 # package all projects for pilot
-tng-validate -s --project tng-smpilot-ns1-emulator/  # sytax validation only (as long as we have to skip integrety validation for CNFs)
-tng-pkg -p tng-smpilot-ns1-emulator  --skip-validation  # skip integrety validation until validator supports CNFs
+tng-pkg -p tng-smpilot-ns1-emulator
