4500035dd5a5cba4354ab731feb3fe87acdd8154
peusterm <manuel.peuster@uni-paderborn.de>
2019-01-28 13:07:08 +0100

    Added Dockerfiles for K8s VNF images #72
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- vnfs/build_docker_vnfs.sh
+++ vnfs/build_docker_vnfs.sh
@@ -4,6 +4,22 @@ set -e
 target_repo=${1-sonatanfv}
 
 #
+# VNFs for Kubernets
+#
+# RTR
+docker build -t $target_repo/vnf-rtr-nat:k8s -f rtr-nat-ubuntu-docker/containers/Dockerfile rtr-nat-ubuntu-docker/containers/
+# CC-CDU01: broker
+docker build -t $target_repo/vnf-cc-broker:k8s -f cc-cloudconnector-docker/containers/cdu_broker/Dockerfile cc-cloudconnector-docker/containers/cdu_broker/
+# CC-CDU02: processor
+docker build -t $target_repo/vnf-cc-processor:k8s -f cc-cloudconnector-docker/containers/cdu_processor/Dockerfile cc-cloudconnector-docker/containers/cdu_processor/
+# MDC
+docker build -t $target_repo/vnf-mdc:k8s -f mdc-machinedatacollector-docker/containers/Dockerfile mdc-machinedatacollector-docker/containers/
+# DT
+docker build -t $target_repo/vnf-dt:k8s -f dt-digitaltwin-docker/containers/Dockerfile dt-digitaltwin-docker/containers/
+# EAE
+docker build -t $target_repo/vnf-eae:k8s -f eae-edgeanalyticsengine-docker/containers/Dockerfile eae-edgeanalyticsengine-docker/containers/
+
+#
 # VNFs for vim-emu
 #
 # RTR
@@ -18,8 +34,3 @@ docker build -t $target_repo/vnf-mdc:vimemu -f mdc-machinedatacollector-docker/c
 docker build -t $target_repo/vnf-dt:vimemu -f dt-digitaltwin-docker/containers/Dockerfile.vimemu dt-digitaltwin-docker/containers/
 # EAE
 docker build -t $target_repo/vnf-eae:vimemu -f eae-edgeanalyticsengine-docker/containers/Dockerfile.vimemu eae-edgeanalyticsengine-docker/containers/
-
-#
-# VNFs for Kubernets
-#
-# TODO
