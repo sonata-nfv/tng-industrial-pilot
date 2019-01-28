e2445f35df788a6b69752df8e74f7196f080436e
peusterm <manuel.peuster@uni-paderborn.de>
2019-01-28 17:27:17 +0100

    Try to debug validate issue #49
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- sdk-projects/pack.sh
+++ sdk-projects/pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 # package all projects for pilot
-tng-pkg -p tng-smpilot-ns1-emulator
-tng-pkg -p tng-smpilot-ns2-emulator
-tng-pkg -p tng-smpilot-ns1-k8s
+tng-pkg -p tng-smpilot-ns1-emulator -v
+tng-pkg -p tng-smpilot-ns2-emulator -v
+tng-pkg -p tng-smpilot-ns1-k8s -v
