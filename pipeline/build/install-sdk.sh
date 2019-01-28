2ba8c1232719dd1b1a4e1a0058bd2d122dbf4304
peusterm <manuel.peuster@uni-paderborn.de>
2019-01-28 17:41:21 +0100

    Re-try
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/build/install-sdk.sh
+++ pipeline/build/install-sdk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 # We need the 5GTANGO SDK tools to package the projects.
-sudo pip3 remove tng-sdk-package
-sudo pip3 remove tng-sdk-validate
-sudo pip3 remove tng-sdk-proejct
+sudo pip3 uninstall tng-sdk-package
+sudo pip3 uninstall tng-sdk-validate
+sudo pip3 uninstall tng-sdk-proejct
 set -e
 sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-project.git
 sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-validation.git
