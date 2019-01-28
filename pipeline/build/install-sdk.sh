27f4430d41a6445142cfebe7b8189a97f052a23b
peusterm <manuel.peuster@uni-paderborn.de>
2019-01-28 15:47:36 +0100

    Install as root
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/build/install-sdk.sh
+++ pipeline/build/install-sdk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 # We need the 5GTANGO SDK tools to package the projects.
-pip3 install git+https://github.com/sonata-nfv/tng-sdk-project.git
-pip3 install git+https://github.com/sonata-nfv/tng-sdk-validation.git
-pip3 install git+https://github.com/sonata-nfv/tng-sdk-package
+sudo pip3 install git+https://github.com/sonata-nfv/tng-sdk-project.git
+sudo pip3 install git+https://github.com/sonata-nfv/tng-sdk-validation.git
+sudo pip3 install git+https://github.com/sonata-nfv/tng-sdk-package
 
 tng-project -h
 tng-validate -h
