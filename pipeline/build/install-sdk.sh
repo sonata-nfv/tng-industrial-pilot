1edb9dbac6f110553d4f0283bfa45656c4a5c805
peusterm <manuel.peuster@uni-paderborn.de>
2019-02-27 14:35:31 +0100

    Further CI fix. Pip sucks.
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/build/install-sdk.sh
+++ pipeline/build/install-sdk.sh
@@ -2,13 +2,13 @@
 #
 # We need the 5GTANGO SDK tools to package the projects.
 # uninstall SDK if present (to ensure we have the latest version)
-sudo pip3 uninstall tngsdk.project
-sudo pip3 uninstall tngsdk.validate
-sudo pip3 uninstall tngsdk.package
+sudo pip3 uninstall --yes tngsdk.project
+sudo pip3 uninstall --yes tngsdk.validate
+sudo pip3 uninstall --yes tngsdk.package
 set -e
-sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-project.git
-sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-validation.git
-sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-package
+sudo pip3 install --upgrade --yes git+https://github.com/sonata-nfv/tng-sdk-project.git
+sudo pip3 install --upgrade --yes git+https://github.com/sonata-nfv/tng-sdk-validation.git
+sudo pip3 install --upgrade --yes git+https://github.com/sonata-nfv/tng-sdk-package
 
 tng-project -h
 tng-validate -h
