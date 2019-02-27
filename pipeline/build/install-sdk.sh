9c14b09df4a275bf2df54da40232722135390102
peusterm <manuel.peuster@uni-paderborn.de>
2019-02-27 14:31:16 +0100

    Fix: Ensure latest SDK in CI pipeline
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/build/install-sdk.sh
+++ pipeline/build/install-sdk.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
 # We need the 5GTANGO SDK tools to package the projects.
+# uninstall SDK if present (to ensure we have the latest version)
+sudo pip3 uninstall tngsdk.project
+sudo pip3 uninstall tngsdk.validate
+sudo pip3 uninstall tngsdk.package
 set -e
 sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-project.git
 sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-validation.git
