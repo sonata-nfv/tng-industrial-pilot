a40a810351cbba89cf1508563ec92d3ae3adee38
peusterm <manuel.peuster@uni-paderborn.de>
2019-02-27 14:52:51 +0100

    Ci test
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/build/install-sdk.sh
+++ pipeline/build/install-sdk.sh
@@ -7,9 +7,15 @@ sudo pip3 uninstall --yes tngsdk.validate
 sudo pip3 uninstall --yes tngsdk.package
 set -e
 sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-project.git
-sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-validation.git
+#sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-validation.git
+git clone https://github.com/sonata-nfv/tng-sdk-validation.git
+cd tng-sdk-validation
+sudo python3 setup.py develop
+cd ..
 sudo pip3 install --upgrade git+https://github.com/sonata-nfv/tng-sdk-package
 
 tng-project -h
 tng-validate -h
 tng-package -h
+
+which tng-validate
--- sdk-projects/pack.sh
+++ sdk-projects/pack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 # directly call the validator to have different validation levels for different projects
-#tng-validate --project tng-smpilot-ns1-emulator -i
-#tng-validate --project tng-smpilot-ns2-emulator -i
-#tng-validate --project tng-smpilot-ns1-k8s -s # TODO -i still buggy
+tng-validate --project tng-smpilot-ns1-emulator -i
+tng-validate --project tng-smpilot-ns2-emulator -i
+tng-validate --project tng-smpilot-ns1-k8s -s # TODO -i still buggy
 # package all projects for pilot (we skip the validation, because we did it already by directly calling the validator)
 tng-pkg -p tng-smpilot-ns1-emulator --skip-validation
 tng-pkg -p tng-smpilot-ns2-emulator --skip-validation
