57fbf12f7d12e52bb9b5bcf062275a17407e9649
peusterm <manuel.peuster@uni-paderborn.de>
2019-02-27 14:03:46 +0100

    Fix: Old demo pilot works again on emulator
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- sdk-projects/instantiate-ns2.sh
+++ sdk-projects/instantiate-ns2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo curl -X GET http://127.0.0.1:5000/instantiations
 curl -X POST http://127.0.0.1:5000/instantiations -d '{"service_name": "tng-smpilot-ns2-emulator"}'
-sudo ovs-vsctl set port dc2.s1-eth4 tag=2
+sudo ovs-vsctl set port dc2.s1-eth4 tag=1
 
 
 
