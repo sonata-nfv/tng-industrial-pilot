bee1f983dc0ca5da91c6730e7750ad831f3088ad
peusterm <manuel.peuster@uni-paderborn.de>
2019-03-01 16:53:12 +0100

    Finally integrated Prometheus DB as local storage for metrics into CC. Major step towards #92. Needs some more testing bevor it is done.
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- vnfs/cc-cloudconnector-docker/containers/cdu_processor/CC_VNF/prometheus.py
+++ vnfs/cc-cloudconnector-docker/containers/cdu_processor/CC_VNF/prometheus.py
@@ -35,8 +35,13 @@ class CcPrometheusClient(object):
         self.registry = CollectorRegistry()
         self.metrics = dict()
         self.gw_host = os.getenv("PUSH_GATEWAY", "localhost:9091")
+        print("CPC: Inititalized Prometheus push client for gateway: {}"
+              .format(self.gw_host))
 
-    def push(self, metric, value):
+    def _push(self, metric, value):
+        """
+        Internal push implementation.
+        """
         # if we do not have a collector for the given metric yet, we create one
         if metric not in self.metrics:
             m = Gauge(
@@ -47,10 +52,46 @@ class CcPrometheusClient(object):
         print("CPC: Setting metric '{}' to {}".format(metric, value))
         try:
             m.set(float(value))
-            print("CPC: Pushing to gateway: {}".format(self.gw_host))
+            print("CPC: Pushing to Prometheus gateway: {}".format(self.gw_host))
             push_to_gateway(self.gw_host, job="cc", registry=self.registry)
         except BaseException as ex:
             print("CPC: Error, couldn't push to Prometheus: {}".format(ex))
+
+    def push_to_db(self, topic, payload):
+        """
+        Public (safe) push method to be used in CC.
+        """
+        metric, value = self._topic_to_metric(topic, payload)
+        if metric is None or value is None:
+            print("CPC: Skipping: {}, {}".format(topic, payload))
+            return  # skip
+        print("CPC: Try to push ({}, {}) to Prometheus GW ...".format(metric, value), flush=True)
+        try:
+            self._push(metric, value)
+            print("done.")
+        except BaseException as ex:
+            print("fail. ({})".format(ex))
+
+    def _topic_to_metric(self, topic, payload):
+        """
+        True to translate topics and payload to Prometheus-safe metric, value.
+        If not, print warning and return None, None.
+        Return metric, value
+        """
+        # TODO this method can be improved a lot!
+        # - build in fixed translations from strings/states to numbers
+        # - build in parsing/translation for hex values
+        try:
+            metric = str(topic).replace("/", "_")
+            metric = metric.replace("@", "")
+            metric = metric.strip(" .,-@+")
+            metric = metric.lower()
+            value = float(payload)
+            return metric, value
+        except BaseException as ex:
+            print("CPC: Couldn't translate to metric, value: {}, {}".format(topic, payload))
+            print("CPC: Problem: {}".format(ex)) 
+        return None, None
 
 
 if __name__ == "__main__":
@@ -59,5 +100,5 @@ if __name__ == "__main__":
     while True:
         mlist = ["metric1", "metric2", "metric3", "metric4"]
         # randomly fill the metrics with values
-        cpc.push(random.choice(mlist), 100*random.random())
+        cpc._push(random.choice(mlist), 100*random.random())
         time.sleep(2)
