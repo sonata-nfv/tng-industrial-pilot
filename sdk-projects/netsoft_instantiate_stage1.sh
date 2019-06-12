51701bbc29b2409c8b95e60840734aad61996df5
peusterm <manuel.peuster@uni-paderborn.de>
2019-06-12 16:29:11 +0200

    Renamed and split helper scripts for NetSoft19 demo
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- sdk-projects/netsoft_instantiate_stage1.sh
+++ sdk-projects/netsoft_instantiate_stage1.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo "On-boarding NS1 and NS2 ..."
-tng-cli -u http://127.0.0.1 package -u eu.5gtango.tng-smpilot-ns1-emulator.0.1.tgo
-tng-cli -u http://127.0.0.1 package -u eu.5gtango.tng-smpilot-ns2-emulator.0.1.tgo
-echo "DONE"
-echo ""
-
 echo "Instantiating NS1.0 and NS1.1 ..."
 tng-cli -u http://127.0.0.1 service -i tng-smpilot-ns1-emulator
 tng-cli -u http://127.0.0.1 service -i tng-smpilot-ns1-emulator
--- sdk-projects/netsoft_instantiate_stage2.sh
+++ sdk-projects/netsoft_instantiate_stage2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "Instantiating NS1.2 ..."
 tng-cli -u http://127.0.0.1 service -i tng-smpilot-ns1-emulator
 echo "DONE"
