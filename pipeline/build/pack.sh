dff7bf01d4e86be602e890f44e9da84f1415209a
peusterm <manuel.peuster@uni-paderborn.de>
2019-06-21 14:53:39 +0200

    Migrated Aurelio's VnV tests to this repo
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/build/pack.sh
+++ pipeline/build/pack.sh
@@ -6,6 +6,11 @@ which tng-validate
 which tng-package
 BASE_DIR="$(pwd)"
 cd sdk-projects/
-# trigger VNF build script
+# trigger service package scripts
+./pack.sh
+cd $BASE_DIR
+
+# trigger test package scripts
+cd vnv-tests/test-packages/
 ./pack.sh
 cd $BASE_DIR
---
+++ vnv-tests/README.md
@@ -0,0 +1,3 @@
+# vnv-tests
+
+Everything related and needed to test this pilot on the 5GTANGO V&V platform.
