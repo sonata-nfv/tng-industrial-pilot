911cf75ab39eb11f0a2b7f41daa073c10f741d2e
peusterm <manuel.peuster@uni-paderborn.de>
2019-06-21 15:01:41 +0200

    Testing CI
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/build/pack.sh
+++ pipeline/build/pack.sh
@@ -7,10 +7,12 @@ which tng-package
 BASE_DIR="$(pwd)"
 cd sdk-projects/
 # trigger service package scripts
+echo "Packaging services and VNFs ..."
 ./pack.sh
 cd $BASE_DIR
 
 # trigger test package scripts
+echo "Packaging V&V tests ..."
 cd vnv-tests/test-packages/
 ./pack.sh
 cd $BASE_DIR
