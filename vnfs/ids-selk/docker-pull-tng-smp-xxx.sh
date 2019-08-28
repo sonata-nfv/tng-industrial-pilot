ca9e9750993e3321f172810cba86852195a00ba8
Alberto Rocha <arocha@ptinovacao.pt>
2019-08-28 07:53:42 +0000

    ENV vars for ES/KB

--- vnfs/ids-selk/docker-pull-tng-smp-xxx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker pull sonatanfv/vnf-ids-kibana
-docker pull sonatanfv/vnf-ids-logstash
-docker pull sonatanfv/vnf-ids-suricata
-docker pull sonatanfv/vnf-ids-filebeat
-docker pull sonatanfv/vnf-ids-elasticsearch
-docker pull sonatanfv/vnf-ids-http
-
---
+++ vnfs/ids-selk/docker-pull-vnf-ids-xxx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker pull sonatanfv/vnf-ids-kibana:latest
+docker pull sonatanfv/vnf-ids-logstash:latest
+docker pull sonatanfv/vnf-ids-suricata:latest
+docker pull sonatanfv/vnf-ids-filebeat:latest
+docker pull sonatanfv/vnf-ids-elasticsearch:latest
+docker pull sonatanfv/vnf-ids-http:latest
+
