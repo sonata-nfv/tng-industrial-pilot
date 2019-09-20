fabca5f98ce64a9d1bb20bdad70038cddf319ef9
Alberto Rocha <arocha@ptinovacao.pt>
2019-09-20 16:55:23 +0000

    add VERSION to start.sh

--- vnfs/ids-selk/l/start.sh
+++ vnfs/ids-selk/l/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export IP=`env | grep EU_5GTANGO_E_VNF3_0_5 | grep 9200_TCP_ADDR | cut -d "=" -f 2`
+VERSION=`echo $1 | tr '.' '_'`; echo $VERSION
+
+export IP=`env | grep EU_5GTANGO_E_VNF3_$VERSION | grep 9200_TCP_ADDR | cut -d "=" -f 2`
 echo $IP
 export ELASTICSEARCH_HOSTS=http://$IP:9200
 bin/logstash
