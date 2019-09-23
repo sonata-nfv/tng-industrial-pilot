eeb1ca66b920ae49274889140b01b7b98f52ce86
Alberto Rocha <arocha@ptinovacao.pt>
2019-09-23 09:56:59 +0000

    major vs minor case on ENV vars

--- vnfs/ids-selk/k/start.sh
+++ vnfs/ids-selk/k/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-export VERSION=`echo $1 | tr '.' '_'`; echo $VERSION
+export VERSION=`echo $1 | tr '.' '_'`
+echo $VERSION
+
 export IP=`env | grep EU_5GTANGO_E_VNF3_$VERSION | grep 9200_TCP_ADDR | cut -d "=" -f 2`
-echo $IP
 export ELASTICSEARCH_HOSTS=http://$IP:9200
+echo $ELASTICSEARCH_HOSTS
+
 bin/kibana -e http://$IP:9200
 
