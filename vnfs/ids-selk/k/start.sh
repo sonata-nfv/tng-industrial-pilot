da758d20934ac95287e26999d4bb1bdcf98b8af8
Alberto Rocha <arocha@ptinovacao.pt>
2019-09-24 12:39:05 +0000

    back to minor case ENV vars - using IPADDR from MetalLB pool

--- vnfs/ids-selk/k/start.sh
+++ vnfs/ids-selk/k/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export IP=`env | grep e_vnf3_eu_5gtango_0_6_elastic9200_ip | cut -d "=" -f 2`
+export IP=`env | grep e_vnf3_eu_5gtango | grep elastic9200_ip | cut -d "=" -f 2`
 export ELASTICSEARCH_HOSTS=http://$IP:9200
 echo $ELASTICSEARCH_HOSTS
 
