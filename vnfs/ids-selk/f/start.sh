b4dd4bf1d95449c1f4a4d54b70f5b4888208a3ea
Alberto Rocha <arocha@ptinovacao.pt>
2019-09-24 10:56:04 +0000

    back to minor case ENV vars - using IPADDR from MetalLB pool

--- vnfs/ids-selk/f/start.sh
+++ vnfs/ids-selk/f/start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-export VERSION=`echo $1 | tr '.' '_'`
-echo $VERSION
-
-export LIP=`env | grep EU_5GTANGO_LHC_VNF2_$VERSION | grep PORT_5044_TCP_ADDR | cut -d "=" -f 2`
+export LIP=`env | grep lhc_vnf2_eu_5gtango_0_6_logstash5044_ip | cut -d "=" -f 2`
 export LOGSTASH_HOSTS=$LIP:5044
 echo $LOGSTASH_HOSTS
 
-export KIP=`env | grep EU_5GTANGO_K_VNF4_$VERSION | grep PORT_5601_TCP | cut -d "=" -f 2`
+export KIP=`env | grep k_vnf4_eu_5gtango_0_6_kibana5601_ip | cut -d "=" -f 2`
 export KIBANA_HOST=$KIP:5601
 echo $KIBANA_HOST
 
