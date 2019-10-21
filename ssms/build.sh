6409bc3db83191886071e051ab4f0772bf95c6fa
peusterm <manuel.peuster@uni-paderborn.de>
2019-10-21 10:24:36 -0300

    Didsable cache
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- ssms/build.sh
+++ ssms/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t sonatanfv/tng-ssm-industry-pilot-ns2 -f ns2-ssm/Dockerfile .    
+docker build --no-cache -t sonatanfv/tng-ssm-industry-pilot-ns2 -f ns2-ssm/Dockerfile .    
