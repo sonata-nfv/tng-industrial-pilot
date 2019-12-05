5b91f7c1ce51988411a74796e62f965095233b68
kaiha <kaiha@mail.upb.de>
2019-12-05 19:35:15 +0100

    to client3

--- vnfs/usecase3/containers/proxyvpn/start.sh
+++ vnfs/usecase3/containers/proxyvpn/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 squid -N -f /etc/squid/squid.conf -z &
 squid -f /etc/squid/squid.conf -NYCd 1 &
-openvpn --config  /etc/openvpn/weidmueller-client.ovpn
+openvpn --config  /etc/openvpn/client3.ovpn
