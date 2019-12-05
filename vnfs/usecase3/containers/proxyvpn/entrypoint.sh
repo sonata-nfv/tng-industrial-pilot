0a1a378d41c078d8a66d0fab2b9987a71b915a92
Kai Hannemann <kai.arne.hannemann28@googlemail.com>
2019-12-05 12:32:44 +0100

    proxy and vpn in one container

--- vnfs/usecase3/containers/proxyvpn/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-squid -N -f /etc/squid/squid.conf -z
-squid -f /etc/squid/squid.conf -NYCd 1
---
+++ vnfs/usecase3/containers/proxyvpn/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+squid -N -f /etc/squid/squid.conf -z &
+squid -f /etc/squid/squid.conf -NYCd 1 &
+openvpn --config  /etc/openvpn/weidmueller-client.ovpn
