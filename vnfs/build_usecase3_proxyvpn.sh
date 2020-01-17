3ec3c3b3beb03f7168177955066ac87490e6ad5e
Stefan Schneider <stefan.schneider@upb.de>
2020-01-17 12:47:49 +0100

    updated build scripts for uc3

---
+++ vnfs/build_usecase3_proxyvpn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script to build all VNFs used for the emulator demo
+set -e
+target_repo=${1-sonatanfv}
+
+#
+# VNFs for Kubernets
+# tag with :k8s and :latest to ensure auto updates by kubernetes
+#
+# usecase3
+docker build -t $target_repo/vnf-proxyvpn:latest -f usecase3/containers/proxyvpn/Dockerfile usecase3/containers/proxyvpn
+# docker build -t $target_repo/vnf-proxyvpn:latest -f usecase3/containers/proxyvpn/Dockerfile usecase3/containers/proxy
+# docker build -t $target_repo/vnf-socat:latest -f usecase3/containers/socat/Dockerfile usecase3/containers/socat
